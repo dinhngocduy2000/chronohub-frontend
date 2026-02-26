8fd713f99a25d72a568b7e8d6b1a3fd2783f560c
dinhngocduy2000 <ngocduydinh2000@gmail.com>
2026-02-27 00:30:25 +0700

    feat: init tanstack router project

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import ReactDOM from 'react-dom/client'
+import { Provider as ReduxProvider } from 'react-redux'
+import { queryClient } from './queries'
+import { routeTree } from './routeTree.gen'
+import { store } from './stores'
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')
+
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ReduxProvider>,
+  )
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { MainLayout } from '@/components/layouts/MainLayout'
+
+import '../styles.css'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <MainLayout>
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'TanStack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </MainLayout>
+  )
+}
