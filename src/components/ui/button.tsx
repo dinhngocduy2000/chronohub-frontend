23e5ede894a8223bb771a779360b9c17751c15ac
dinhngocduy2000 <ngocduydinh2000@gmail.com>
2026-03-08 23:50:54 +0700

    chore: add loading state for buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from 'radix-ui'
 import type * as React from 'react'
 
 import { cn } from '@/lib/utils'
+import { Spinner } from './spinner'
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -41,10 +42,13 @@ function Button({
   variant = 'default',
   size = 'default',
   asChild = false,
+  loading = false,
+  children,
   ...props
 }: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
+    loading?: boolean
   }) {
   const Comp = asChild ? Slot.Root : 'button'
 
@@ -55,7 +59,10 @@ function Button({
       data-size={size}
       className={cn('cursor-pointer', buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      {children}
+      {loading && <Spinner />}
+    </Comp>
   )
 }
 
